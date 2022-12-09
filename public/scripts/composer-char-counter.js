ddc82d6946abd4b62c345428acad9b426f792575
Amina Malik <amina.malik@telus.com>
2022-12-09 18:17:04 -0500

    clean up code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,26 +1,7 @@
-// $(document).ready(function() {
 
-//   const $input = $('#tweet-text');
-//   const $counter = $('.counter');
-
-//   $input.bind('keydown, keyup', function(e) {
-
-//     const $tweetLength = $input.val().length;
-
-//       $counter.text(140 - $tweetLength);
-
-//       if ($tweetLength > 140) {
-//         $counter.addClass('counterRed');
-//       } else {
-//         $counter.removeClass('counterRed');
-//       }
-
-//   });
-
-// });
 $(document).ready(function() {
   // let counter = $('#counter');
-  // console.log('composer-char-counter is loaded into indexed.html');  //not showing on console!!!
+
   
   $("#tweet-text").on("input", function() {
     const maxLength = 140;
@@ -32,7 +13,7 @@ $(document).ready(function() {
     let currentCount = $('.currentCounter');
     currentCount.html(maxLength);
 
-    // how do I do this in a css file?
+    
     if (textRemaining < 0) {
       counter.css('color', '#FF0000');
       currentCount.css('color', '#FF0000');
